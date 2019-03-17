a23b0ac2ee5fa59ae941c7678a7d8a9b8adb28ad
Jonathan Brown <jbrown@mix-blockchain.org>
2019-03-17 16:04:25 +0700

    Support receiving ERC1155 tokens.

--- src/Account.sol
+++ src/Account.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.4;
 
 import "mix-token/MixToken.sol";
+import "./IERC1155TokenReceiver.sol";
 
 
 /**
@@ -8,7 +9,7 @@ import "mix-token/MixToken.sol";
  * @author Jonathan Brown <jbrown@mix-blockchain.org>
  * @dev Contract for each MIX account.
  */
-contract Account is MixTokenReceiverInterface {
+contract Account is MixTokenReceiverInterface, IERC1155TokenReceiver {
 
     /**
      * @dev Controller of the account.
@@ -35,6 +36,8 @@ contract Account is MixTokenReceiverInterface {
      * @param tokenContract The ERC223 contract that manages the token.
      */
     event ReceiveToken(address from, uint value, address tokenContract);
+
+    event ReceiveERC1155Token(address operator, address from, uint256 id, uint256 value);
 
     /**
      * @dev Revert if the controller of the account is not the sender.
@@ -118,13 +121,48 @@ contract Account is MixTokenReceiverInterface {
     /**
      * @dev MixToken fallback function.
      */
-     function receiveMixToken(address from, uint value, bytes calldata) external returns (bytes4) {
-         // Log the event.
-         if (from != controller) {
-             emit ReceiveToken(from, value, msg.sender);
-         }
-         return 0xf2e0ed8f;
+    function receiveMixToken(address from, uint value, bytes calldata) external returns (bytes4) {
+        // Log the event.
+        if (from != controller) {
+            emit ReceiveToken(from, value, msg.sender);
+        }
+        return 0xf2e0ed8f;
      }
+
+    /**
+     * @dev An ERC1155-compliant smart contract MUST call this function on the token recipient contract, at the end of a `safeTransferFrom` after the balance has been updated.
+     * @param operator The address which called the `safeTransferFrom` function
+     * @param from The address which previously owned the token
+     * @param id The id of the token being transferred
+     * @param value The amount of tokens being transferred
+     * @return `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))`
+    */
+    function onERC1155Received(address operator, address from, uint256 id, uint256 value, bytes calldata) external returns (bytes4) {
+        // Log the event.
+        if (from != controller) {
+            emit ReceiveERC1155Token(operator, from, id, value);
+        }
+        return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
+    }
+
+    /**
+     * @dev An ERC1155-compliant smart contract MUST call this function on the token recipient contract, at the end of a `safeBatchTransferFrom` after the balances have been updated.
+     * @param operator The address which called the `safeBatchTransferFrom` function
+     * @param from The address which previously owned the token
+     * @param ids An array containing ids of each token being transferred
+     * @param values An array containing amounts of each token being transferred
+     * @return `bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"))`
+    */
+    function onERC1155BatchReceived(address operator, address from, uint256[] calldata ids, uint256[] calldata values, bytes calldata) external returns (bytes4) {
+        // Log the event.
+        if (from != controller) {
+            uint count = ids.length;
+            for (uint i = 0; i < count; i++) {
+                emit ReceiveERC1155Token(operator, from, ids[i], values[i]);
+            }
+        }
+        return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
+    }
 
     /**
      * @dev Send all MIX to the controller.
