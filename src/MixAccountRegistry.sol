92ea2e0faf24af24913134ec780d9949549e954a
Jonathan Brown <jbrown@mix-blockchain.org>
2019-05-31 01:20:25 +0700

    Implement MixAccountRegistry

---
+++ src/MixAccountRegistry.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.5.7;
+
+import "./MixAccount.sol";
+
+
+/**
+ * @title MixAccountRegistry
+ * @author Jonathan Brown <jbrown@mix-blockchain.org>
+ * @dev Allows controllers to store a record of which accounts they control.
+ */
+contract MixAccountRegistry {
+
+    /**
+     * @dev Mapping of controller to array of account contracts.
+     */
+    mapping (address => MixAccount[]) controllerAccounts;
+
+    modifier senderIsController(MixAccount account) {
+        require (account.getController() == msg.sender, "Sender is not controller.");
+        _;
+    }
+
+    modifier accountNotAdded(MixAccount account) {
+        uint i = getAccountIndex(account);
+        require (i == controllerAccounts[msg.sender].length, "Account already added.");
+        _;
+    }
+
+    function getAccountIndex(MixAccount account) internal returns (uint i) {
+        MixAccount[] storage accounts = controllerAccounts[msg.sender];
+        for (i = 0; i < accounts.length; i++) {
+            if (accounts[i] == account) {
+                break;
+            }
+        }
+    }
+
+    function addAccount(MixAccount account) external
+        senderIsController(account)
+        accountNotAdded(account)
+    {
+        controllerAccounts[msg.sender].push(account);
+    }
+
+    function removeAccount(MixAccount account) external {
+        MixAccount[] storage accounts = controllerAccounts[msg.sender];
+        uint i = getAccountIndex(account);
+        require (i != accounts.length, "Account not found.");
+
+        if (i != 0) {
+            accounts[i] = accounts[accounts.length - 1];
+        }
+        controllerAccounts[msg.sender].pop();
+    }
+
+    function getAccounts() external view returns (MixAccount[] memory) {
+        return controllerAccounts[msg.sender];
+    }
+
+}
